31c2576ea233171372205d9e9f17bf9626e7cc0f
muhammad ammar <muhammadammar42000@gmail.com>
2020-06-14 03:40:17 +0500

    fetch user info in dashboard page and display it

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,33 @@
-console.log("connected")
+var firestore = firebase.firestore();
+var auth = firebase.auth();
+var nameDiv = document.querySelector(".name h4");
+//fteching user id form url
+var uid = location.hash.substring(1,location.hash.length);
+// // console.log(data.createdAt.toDate().toISOString().split("T")[0]);
+
+var fetchUserInfo = async (uid) =>  {
+    try {
+        var userInfo = await firestore.collection("user").doc(uid).get();
+        return userInfo.data();
+        
+        
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+fetchUserInfo(uid);
+
+//auth listner
+auth.onAuthStateChanged(async (user) => {
+    if (user) {
+        var {uid} = user;
+        var userInfo = await fetchUserInfo(uid);
+        console.log(userInfo)
+        //setting user info
+        //name
+        nameDiv.textContent = userInfo.fullName;
+    } else {
+        console.log("user logged out")
+    }
+})

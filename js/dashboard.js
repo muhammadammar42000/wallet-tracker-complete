f491928ce13c5d9d6bd77a3baa7914931f3791e7
muhammad ammar <muhammadammar42000@gmail.com>
2020-06-15 00:31:41 +0500

    done with transaction rendering

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,9 +1,44 @@
 var firestore = firebase.firestore();
 var auth = firebase.auth();
 var nameDiv = document.querySelector(".name h4");
+var signoutBtn = document.querySelector(".signoutBtn")
+var transactionList = document.querySelector(".transactionList")
 //fteching user id form url
-var uid = location.hash.substring(1,location.hash.length);
+var uid = null;
 // // console.log(data.createdAt.toDate().toISOString().split("T")[0]);
+
+var transactionForm = document.querySelector(".transactionForm");
+
+var renderTransactions = (transactionArr) => {
+    transactionList.innerHTML= ""
+    transactionArr.forEach((transaction,index) =>{
+        var {title,cost,transactionAt,transactionId }=transaction;
+        transactionList.insertAdjacentHTML('beforeend',
+        `<div class="transactionListItem">
+        <div class="renderIndex listItem">
+          <h3>${++index}</h3>
+        </div>
+        <div class="renderTitle listItem">
+          <h3>${title}</h3>
+        </div>
+        <div class="renderCost listItem">
+          <h3>${cost}</h3>
+        </div>
+        <div class="renderTransactionAt listItem">
+          <h3>${transactionAt.toDate().toISOString().split("T")[0]}</h3>
+        </div>
+        <div class="renderTransactionAt listItem">
+        <a href="./transaction.html#${transactionId}"><button type="button">view</buttom></a>
+        </div>
+      </div>`
+      );
+    })
+
+}
+
+var userSignout = async () =>{
+    await auth.signOut()
+}
 
 var fetchUserInfo = async (uid) =>  {
     try {
@@ -16,18 +51,71 @@ var fetchUserInfo = async (uid) =>  {
     }
 };
 
-fetchUserInfo(uid);
+var transactionFormSubmission = async (e) => {
+    e.preventDefault();
+    try {
+        var title = document.querySelector(".title").value;
+        var cost = document.querySelector(".cost").value;
+        var transactionType = document.querySelector(".transactionType").value;
+        var transactionAt = document.querySelector(".transactionAt").value;
+        if(title && cost && transactionType && transactionAt){
+            var transactionObj = {
+                title,
+                cost,
+                transactionType,
+                transactionAt: new Date(transactionAt),
+                transactionBy : uid
+            }
+            await firestore.collection("transaction").add(transactionObj)
+            //render fresh transactions
+            //fetch users transaction
+        var transactions = await fetchTransaction(uid);
+        //render process
+    renderTransactions(transactions);
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+var fetchTransaction = async (uid) => {
+    var transactions = [];
+    var query = await firestore
+    .collection("transaction")
+    .where("transactionBy","==",uid)
+    .orderBy("transactionAt","desc")
+    .get();
+    query.forEach((doc) => {
+        transactions.push({...doc.data(),transactionId: doc.id})
+    })
+    return transactions
+}
+
+
+// fetchUserInfo(uid);
+
+signoutBtn.addEventListener("click" , userSignout);
+
+transactionForm.addEventListener("submit",(e) => transactionFormSubmission(e))
 
 //auth listner
 auth.onAuthStateChanged(async (user) => {
     if (user) {
-        var {uid} = user;
+        uid = user.uid;
         var userInfo = await fetchUserInfo(uid);
         console.log(userInfo)
         //setting user info
         //name
         nameDiv.textContent = userInfo.fullName;
+        //render transaction   
+        //fetch users transaction
+        var transactions = await fetchTransaction(uid);
+        //render process
+        // console.log(transactions)
+    renderTransactions(transactions);
     } else {
-        console.log("user logged out")
+        location.assign("./index.html")
+
     }
 })
